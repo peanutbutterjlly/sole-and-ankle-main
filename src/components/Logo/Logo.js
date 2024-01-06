ea444d9ce73332abe6629b05576c9446e2ce8aa9
steve <steverios_sr@yahoo.com>
2024-01-06 11:03:55 -0600

    header 'complete'

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -14,6 +14,10 @@ const Logo = (props) => {
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  margin-right: 0px;
+  position: absolute;
+  left: 32px;
+  top: 78px;
 `;
 
 const Wrapper = styled.h1`
